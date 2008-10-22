58efee9d24cf80d045d079c56418800b164e1f6f
Chris Parrish <chris.parrish-gitmail@swankinnovations.com>
2008-10-22 03:37:47 -0600

    Improved Specs.  Added symbolic elements for many of Radiant's values.

--- app/models/conditional_statement.rb
+++ app/models/conditional_statement.rb
@@ -5,8 +5,9 @@ class ConditionalStatement
               :comparison_element,
               :err_msg
   
-  def initialize(input_text)
+  def initialize(input_text, tag_bindings = nil)
     @input_text = input_text
+    @tag_bindings = tag_bindings
     parse_input_text
   end
  
@@ -33,7 +34,7 @@ class ConditionalStatement
                 '|' +
                 '(nil|null|nothing)' + # nil
                 '|' +
-                '([^\s\]\[]+(?:\[[^\)]*\])?)' + # number or fancy object
+                '([^\s\]\[]+(?:\[[^\]]*\])?)' + # number or fancy object
                 ")" 
       condition_type = "([^\s]+)"
       
@@ -43,36 +44,12 @@ class ConditionalStatement
  
       if diced_conditional.nil? || diced_conditional.length != 11
         @is_valid = false
-        @err_msg = "invalid condition"
+        @err_msg = %{invalid condition "#{@input_text}"}
       elsif
         @is_valid = true
-        @primary_element = evaluate_diced_element(diced_conditional[0, 5])
+        @primary_element = evaluate_element(diced_conditional[0, 5])
         @comparison_type = evaluate_comparison_type(diced_conditional[5]) if valid?
-        @comparison_element = evaluate_diced_element(diced_conditional[6, 5]) if valid?
-      end
-    end
-
-
-    def evaluate_diced_element(diced_element)
-      if diced_element[0]
-        diced_element[0].gsub("''", "'")
-      elsif diced_element[1]
-        Regexp.new(diced_element[1])
-      elsif diced_element[2]
-        diced_element[2].downcase == "true"
-      elsif diced_element[3]
-        nil
-      elsif diced_element[4]
-        # try to cast as float
-        if (Float(diced_element[4]) rescue false)
-          diced_element[4].to_f
-        else
-          #here's our fancy schmancy element... 
-          diced_element[4]
-          nil
-        end
-      else
-        nil
+        @comparison_element = evaluate_element(diced_conditional[6, 5]) if valid?
       end
     end
 
@@ -93,17 +70,91 @@ class ConditionalStatement
           ">="
         when 'lte'
           "<="
-        when 'is_not', 'not_equals', '!=', '<>'
-          "!="
+#        when 'is_not', 'not_equals', '!=', '<>'
+#          "!="
         when 'is', 'equals', '==', '='
           "=="
         when 'matches', '=~'
           "=~"
         else
           @is_valid = false
-          @err_msg = "invalid comparison (#{comparison_type}) in condition"
+          @err_msg = %{invalid comparison "#{comparison_type}" in condition "#{@input_text}"}
           nil
       end
-      
     end
+
+
+    def evaluate_element(element_array)
+      if element_array[0]
+        element_array[0].gsub("''", "'")
+      elsif element_array[1]
+        Regexp.new(element_array[1])
+      elsif element_array[2]
+        element_array[2].downcase == "true"
+      elsif element_array[3]
+        nil
+      elsif element_array[4]
+        # try to cast as float
+        if (Float(element_array[4]) rescue false)
+          element_array[4].to_f
+        else
+          #here's our fancy schmancy element... 
+          evaluate_symbolic_element element_array[4]
+        end
+      else
+        nil
+      end
+    end
+
+
+    def evaluate_symbolic_element(element_string)
+      if (/^(\w+(?:\.\w+)*)(?:\[([^\]]*)\])?$/).match(element_string).nil?
+        @is_valid = false
+        @err_msg = %{invalid syntax for element "#{element_string}" in condition "#{@input_text}"}
+        nil
+      else
+        case $1
+          when "title", "slug", "url", "breadcrumb", "author"
+            @tag_bindings.page.send($1)
+
+          when "content" #, part
+            part = $2
+            part = "body" if part.blank?
+            if @tag_bindings.page.part(part)
+              @tag_bindings.page.part(part).content
+            else
+              nil
+            end
+            
+          when "parts"
+            output = ""
+            @tag_bindings.page.parts.each do |part|
+              output << part.name + ";"
+            end
+            output.chop
+
+          when "parts.count"
+            @tag_bindings.page.parts.length
+            
+          when "children.count"
+            
+          when "mode"
+            if (dev_host = Radiant::Config['dev_host']) && (@tag_bindings.page.request.host == dev_host)
+              "dev"
+            elsif @tag_bindings.page.request.host =~ /^dev\./
+              "dev"
+            else
+              "live"
+            end
+          
+          else
+            @is_valid = false
+            @err_msg = %{unable to evaluate element "#{element_string}" in condition "#{@input_text}"}
+            nil
+        end
+      end
+    end
+
+  
+  
 end
--- spec/models/conditional_statement_spec.rb
+++ spec/models/conditional_statement_spec.rb
@@ -1,13 +1,23 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
+
+def build_input_using(element_value, element_type = :primary_element)
+  if element_type == :primary_element
+    input_string = element_value.to_s + " == 'ignore this'"
+  else
+    input_string = "'ignore this' == " + element_value.to_s        
+  end
+end
+
+
 describe ConditionalStatement do
-
-#  scenario :pages
-
+  
   it "should not permit insantiation without a paramter" do
     lambda{conditional_statement = ConditionalStatement.new}.
         should raise_error(ArgumentError)
   end
+
+
 
 
   describe "with valid input" do
@@ -16,10 +26,10 @@ describe ConditionalStatement do
       { :input => "10 is 5", :output => "==" },
       { :input => "10 equals 5", :output => "==" },
       
-      { :input => "10 != 5", :output => "!=" },
-      { :input => "10 <> 5", :output => "!=" },
-      { :input => "10 is_not 5", :output => "!=" },
-      { :input => "10 not_equals 5", :output => "!=" },
+#      { :input => "10 != 5", :output => "!=" },
+#      { :input => "10 <> 5", :output => "!=" },
+#      { :input => "10 is_not 5", :output => "!=" },
+#      { :input => "10 not_equals 5", :output => "!=" },
       
       { :input => "10 =~ 5", :output => "=~" },
       { :input => "10 matches 5", :output => "=~" },
@@ -65,8 +75,6 @@ describe ConditionalStatement do
     end
   
  
- 
- 
     [ { :input => "'string'", :output => "string" },
       { :input => " 'a string' ", :output => "a string" },
       { :input => "  'a very long string'", :output => "a very long string" },
@@ -95,17 +103,10 @@ describe ConditionalStatement do
       { :input => "-5.25   ", :output => -5.25 },
       { :input => "   +101.25", :output => 101.25 }
     ].each do |current_element|
-
       [ :primary_element, :comparison_element ].each do |element_type|
 
-        # build the input string inserting the element in the proper location
-        # depending on whether we're testing the primary or comparison element
-        if element_type == :primary_element
-          input_string = current_element[:input] + " == 'ignore this'"
-        else
-          input_string = "'ignore this' == " + current_element[:input]          
-        end
-        
+        input_string = build_input_using(current_element[:input], element_type)
+
         describe "where the #{element_type.to_s.humanize.titlecase} is a literal (#{input_string})" do
 
           before :all do
@@ -128,15 +129,39 @@ describe ConditionalStatement do
   end
 
 
-  describe "with invalid input" do
-    
-    [ { :input => "", :err_msg => "invalid condition" },
-      { :input => "a b c d", :err_msg => "invalid condition" },
-      { :input => "'abc' /abc/", :err_msg => "invalid condition" },
-      { :input => "100", :err_msg => "invalid condition" },
-      { :input => "100 ??? 12", :err_msg => "invalid comparison (???) in condition" },
-      { :input => "abc.def[ghi]", :err_msg => "could not evaluate \"abc.def[ghi]\" in condition" },
-      { :input => "jabber = wocky", :err_msg => "could not evaluate \"jabber\" in condition" }
+
+  
+  describe "with malformed conditions" do
+    [ "",
+      "a b c d",
+      "100",
+    ].each do |condition|
+      
+      describe "(#{condition})" do
+        
+        before :all do
+          @conditional_statement = ConditionalStatement.new(condition)
+        end
+        
+        it "should not be valid" do
+          @conditional_statement.should_not be_valid
+        end
+        
+        it "should produce the error message: invalid condition \"#{condition}\"" do
+          @conditional_statement.err_msg.should == "invalid condition \"#{condition}\""
+        end
+        
+      end
+  
+    end
+  end
+  
+  
+  
+  
+  describe "with valid input where the primary element" do
+    [ { :input => "'abc' /abc/ 12", :comparison => "/abc/" },
+      { :input => "100 ??? 12", :comparison => "???"  },
     ].each do |condition|
       
       describe "(#{condition[:input]})" do
@@ -149,31 +174,243 @@ describe ConditionalStatement do
           @conditional_statement.should_not be_valid
         end
         
-        it "should offer the proper err_msg" do
-          @conditional_statement.err_msg.should == condition[:err_msg]
+        it "should produce the error message: invalid comparison \"#{condition[:comparison]}\" in condition \"#{condition[:input]}\"" do
+          @conditional_statement.err_msg.should == "invalid comparison \"#{condition[:comparison]}\" in condition \"#{condition[:input]}\""
+        end
+        
+      end
+      
+    end
+  end
+  
+  
+  
+  
+  describe "with conditions having malformed symbolic elements" do
+    [ { :input => "abc(ghi) = 12", :element => "abc(ghi)" },
+      { :input => "abc.[ghi] = 12", :element => "abc.[ghi]" },
+      { :input => "abc..def[ghi] = 12", :element => "abc..def[ghi]" },
+      { :input => "abc.def*[ghi] = 12", :element => "abc.def*[ghi]" },
+      { :input => "ab@c.def[ghi] = 12", :element => "ab@c.def[ghi]" }
+    ].each do |condition|
+
+      describe "(#{condition[:input]})" do
+        
+        before :all do
+          @conditional_statement = ConditionalStatement.new(condition[:input])
+        end
+        
+        it "should not be valid" do
+          @conditional_statement.should_not be_valid
+        end
+        
+        it "should produce the error message: invalid syntax for element \"#{condition[:element]}\" in condition \"#{condition[:input]}\"" do
+          @conditional_statement.err_msg.should == 
+              "invalid syntax for element \"#{condition[:element]}\" in condition \"#{condition[:input]}\""
         end
         
       end
 
     end
-
   end
 
 
-  describe "in a Page Context" do
 
-    before :each do
-      create_page "conditional_tags"
-      @page = pages(:conditional_tags)
+  describe "with conditions containing unknown symbolic elements" do
+    [ { :input => "abc.def[ghi] = 12", :element => "abc.def[ghi]" },
+      { :input => "jabber = wocky", :element => "jabber" },
+      { :input => "jub.jub[bird] = false", :element => "jub.jub[bird]" }
+    ].each do |condition|
+       describe "(#{condition[:input]})" do
+        
+        before :all do
+          @conditional_statement = ConditionalStatement.new(condition[:input])
+        end
+        
+        it "should not be valid" do
+          @conditional_statement.should_not be_valid
+        end
+        
+        it "should produce the error message: unable to evaluate element \"#{condition[:element]}\" in condition \"#{condition[:input]}\"" do
+          @conditional_statement.err_msg.should == 
+              "unable to evaluate element \"#{condition[:element]}\" in condition \"#{condition[:input]}\""
+        end
+        
+      end
+     
+    end
+      
+  end
+
+
+
+    
+  [ :primary_element, :comparison_element].each do |element_type|
+    [:title, :slug, :url, :breadcrumb, :author].each do |page_property|
+
+      describe "when the #{element_type.to_s.humanize.titlecase} is \"#{page_property}\"" do
+        
+        before :all do
+          page = mock("page")
+          page.stub!(:title).and_return('Gettysburg Address')
+          page.stub!(:url).and_return("http://loghome.il.gov")
+          page.stub!(:slug).and_return("/speeches/disregared/war")
+          page.stub!(:breadcrumb).and_return("4 Score +")
+          page.stub!(:author).and_return("Abraham Lincoln")
+          @page_binding = mock("page_binding", :page => page)
+        end
+  
+        it "should return the value for page.#{page_property} (from the given page binding)" do
+          input_string = build_input_using(page_property, element_type)
+          conditional_statement = ConditionalStatement.new(input_string, @page_binding)
+          
+          conditional_statement.send(element_type).should ==
+              @page_binding.page.send(page_property)
+        end
+        
+      end
+ 
+    end
+ 
+    
+    describe "when the #{element_type.to_s.humanize.titlecase} is \"content\"" do
+
+      before :each do
+        page = mock("page")
+        page.stub!(:part).with("body").and_return(mock("body", :content => "My Body's Content"))
+        page.stub!(:part).with("other part").and_return(mock("other", :content => "Some Other Content"))
+        page.stub!(:part).with("another part").and_return(mock("other", :content => "Some More Content"))
+        @page_binding = mock("page_binding", :page => page)
+      end
+      
+      ["body", "other part", "another part"]. each do |part|
+        
+        it "should return the content of the \"#{part}\" part (element is \"content[#{part}]\")" do
+          input_string = build_input_using("content[#{part}]", element_type)
+          conditional_statement = ConditionalStatement.new(input_string, @page_binding)
+          
+          conditional_statement.send(element_type).should ==
+              @page_binding.page.send("part", part).content
+        end
+        
+      end
+        
+      it "should return the content of the \"body\" part  (element is \"content[]\")" do
+        input_string = build_input_using("content[]", element_type)
+        conditional_statement = ConditionalStatement.new(input_string, @page_binding)
+        
+        conditional_statement.send(element_type).should ==
+            @page_binding.page.part("body").content
+      end
+        
+    end
+
+    describe "when the #{element_type.to_s.humanize.titlecase} is \"parts\"" do
+
+      before :each do
+        part1 = mock("part1", :name => "part 1")
+        part2 = mock("part2", :name => "part 2")
+        part3 = mock("part3", :name => "part 3")
+        page = mock("page", :parts => [part1, part2, part3])
+        @page_binding = mock("page_binding", :page => page)
+      end
+      
+      it "should return a \";\" delimited list of the page parts" do
+        input_string = build_input_using("parts", element_type)
+        conditional_statement = ConditionalStatement.new(input_string, @page_binding)
+        
+        conditional_statement.send(element_type).should == "part 1;part 2;part 3"
+      end
+        
+    end
+
+
+    describe "when the #{element_type.to_s.humanize.titlecase} is \"parts.count\"" do
+
+      before :each do
+        part1 = mock("part1", :name => "part 1")
+        part2 = mock("part2", :name => "part 2")
+        part3 = mock("part3", :name => "part 3")
+        page = mock("page", :parts => [part1, part2, part3])
+        @page_binding = mock("page_binding", :page => page)
+      end
+      
+      it "should return a the number of page parts" do
+        input_string = build_input_using("parts.count", element_type)
+        conditional_statement = ConditionalStatement.new(input_string, @page_binding)
+        
+        conditional_statement.send(element_type).should == 3
+      end
+        
+    end
+
+
+  
+#    describe "when the #{element_type.to_s.humanize.titlecase} refers to \"children.count\"" do
+#
+#      before :each do
+#        page = mock("page")
+#        page.stub!(:children).and_return(mock("children", :count => 3))
+#        @page_binding = mock("page_binding", :page => page)
+#      end
+#      
+#      it "should return the number of child pages if element is \"children.count\"" do
+#        input_string = build_input_using("children.count", element_type)
+#        conditional_statement = ConditionalStatement.new(input_string, @page_binding)
+#        
+#        conditional_statement.send(element_type).should ==
+#            @page_binding.page.children.count
+#      end
+#      
+#    end
+
+
+
+
+    describe "when the #{element_type.to_s.humanize.titlecase} is \"mode\"" do
+
+      it "should return \"dev\" if the site is in development mode" do
+        Radiant::Config.stub!("[]").with('dev_host').and_return("some.development.domain")
+        page = mock("page")
+        page.stub!(:request).and_return(mock("host", :host => "some.development.domain"))
+        @page_binding = mock("page_binding", :page => page)
+
+        input_string = build_input_using("mode", element_type)
+        conditional_statement = ConditionalStatement.new(input_string, @page_binding)
+        
+        conditional_statement.send(element_type).should == "dev"
+      end
+      
+      
+      it "should return \"dev\" if dev_host config value is unavailable, and the request.host value is dev.*" do
+        Radiant::Config.stub!("[]").with('dev_host').and_return(nil)
+        page = mock("page")
+        page.stub!(:request).and_return(mock("host", :host => "dev.some.site"))
+        @page_binding = mock("page_binding", :page => page)
+
+        input_string = build_input_using("mode", element_type)
+        conditional_statement = ConditionalStatement.new(input_string, @page_binding)
+        
+        conditional_statement.send(element_type).should == "dev"
+      end
+
+
+      it "should return \"live\" for all other reqest host values" do
+        page = mock("page")
+        page.stub!(:request).and_return(mock("host", :host => "some.site"))
+        @page_binding = mock("page_binding", :page => page)
+
+        input_string = build_input_using("mode", element_type)
+        conditional_statement = ConditionalStatement.new(input_string, @page_binding)
+        
+        conditional_statement.send(element_type).should == "live"
+      end
+
     end
 
   end
     
-#  before(:each) do
-#    @conditional_statement = ConditionalStatement.new
-#  end
-#
-#  it "should be valid" do
-#    @conditional_statement.should be_valid
-#  end
+
+
+
 end
